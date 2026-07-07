84ed9b5694e17e941bd8ad3153dd33be2018899e
iskaziz <iskander.azizuddin@gmail.com>
2026-07-07 20:40:59 +0100

    Add files via upload

--- js/loading-screen.js
+++ js/loading-screen.js
@@ -2,8 +2,6 @@ window.FEISK_LOADING_SCREEN = {
   uiAssets: [
     'assets/ui/feisk-sign.png',
     'assets/ui/hatch-closed.png',
-    'assets/ui/hatch-open.png',
-    'assets/backgrounds/loading-jungle-portrait.png',
-    'assets/backgrounds/loading-jungle-landscape.png'
+    'assets/ui/hatch-open.png'
   ]
 };
